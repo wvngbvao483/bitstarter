f7933cced8fce0b1c0deeb7bfa3c8a773d2b53c8
Biao Wang <wvngbvao483@gmail.com>
2014-02-23 21:19:58 +0000

    Update web.js again

--- web.js
+++ web.js
@@ -2,8 +2,13 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var fs = require('fs');
+var infile = "index.html";
+var out = fs.readFileSync(infile);
+var outstring = out.toString('utf-8');
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(outstring);
 });
 
 var port = process.env.PORT || 5000;
