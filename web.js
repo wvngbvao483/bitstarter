99d7b9650d75488a92c8cb65c4bd897a38f72f35
Biao Wang <wvngbvao483@gmail.com>
2014-02-23 21:01:09 +0000

    Update web.js from index.html

--- web.js
+++ web.js
@@ -1,12 +1,1 @@
-var express = require('express');
-var app = express();
-app.use(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World! This is Biao');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+Hello World from index.html
